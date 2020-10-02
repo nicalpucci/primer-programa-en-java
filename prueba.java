2c04f8808fa76315eae9bb05e8f880e1635b4685
Nicolas Alfonso <nicalpucci@gmail.com>
2020-10-02 18:17:48 +0200

    agrego localidad y año de nacimiento

--- prueba.java
+++ prueba.java
@@ -1,7 +1,6 @@
  public class MiNombre {
      public static void main(String args[]){
          System.out.println("Nicolás Alfonso Pucci, 1999");
-         System.out.println("Caracas");
-         System.out.println("Trobajo del Camino, Windows 10 Home x64");
+         
      }
  }
